5e7ea49d7c7dd74af66677e42e11cfb3a8cfc97f
Fernanda Antunes <fernanda.modeneze@gmail.com>
2022-11-07 00:19:34 -0300

    Jogos do grupo do Brasil

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ function createGame(player1, hour, player2) {
   `
 }
 
-let delay = -0.3;
+let delay = -0.3
 function createCard(date, day, games) {
-  delay = delay + 0.4;
+  delay = delay + 0.4
   return `
     <div class="card" style="animation-delay:${delay}s">
       <h2>${date} <span>${day}</span></h2>
@@ -22,11 +22,27 @@ function createCard(date, day, games) {
 }
 
 document.querySelector("#cards").innerHTML =
-  createCard("24/11", "quinta", createGame("brazil", "16:00", "cameroon")) +
+  createCard(
+    "24/11",
+    "quinta",
+    createGame("switzerland", "07:00", "cameroon") +
+      createGame("uruguay", "10:00", "south-korea") +
+      createGame("portugal", "13:00", "ghana") +
+      createGame("brazil", "16:00", "serbia")
+  ) +
   createCard(
     "28/11",
     "segunda",
-    createGame("brazil", "13:00", "switzerland") +
-      createGame("portugal", "15:00", "uruguay")
+    createGame("cameroon", "07:00", "serbia") +
+      createGame("south-korea", "10:00", "ghana") +
+      createGame("brazil", "13:00", "switzerland") +
+      createGame("portugal", "16:00", "uruguay")
   ) +
-  createCard("02/12", "sexta", createGame("brazil", "16:00", "serbia"))
+  createCard(
+    "02/12",
+    "sexta",
+    createGame("south-korea", "12:00", "portugal") +
+      createGame("ghana", "12:00", "uruguay") +
+      createGame("serbia", "16:00", "switzerland") +
+      createGame("cameroon", "16:00", "brazil")
+  )
